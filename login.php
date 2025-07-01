6f10fcc92e52ebade44e7f59d20d55f021ea5999
NITHYA-RR <nithyaramesh192004@gmail.com>
2025-07-01 23:02:07 +0530

    login file changes

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
 include 'libs/load.php';
 Session::renderpage(); // Ensure this method loads the 'master' template correctly
+// Add any additional logic or content for the login page here
 ?>
 
 
